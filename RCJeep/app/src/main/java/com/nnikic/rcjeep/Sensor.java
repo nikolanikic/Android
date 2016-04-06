8926efb334c5c81cd6b77c8c81f6385aba1f4772
Nikola Nikic <nikolanikic11@gmail.com>
2016-04-06 10:49:12 +0200

    bckg
    Added bckg

---
+++ RCJeep/app/src/main/java/com/nnikic/rcjeep/Sensor.java
@@ -0,0 +1,19 @@
+package com.nnikic.rcjeep;
+
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+/**
+ * Created by nnikic on 1/19/2016.
+ */
+public class Sensor extends MainActivity implements SensorEventListener {
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(android.hardware.Sensor sensor, int accuracy) {
+
+    }
+}
