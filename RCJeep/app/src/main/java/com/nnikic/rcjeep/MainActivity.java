566385bbc4dd35f8d5604c1f224382b34ce7d54d
Nikola Nikic <nikolanikic11@gmail.com>
2016-04-06 16:07:52 +0200

    added splash activity
    Added splash activity and some other stuff

--- RCJeep/app/src/main/java/com/nnikic/rcjeep/MainActivity.java
+++ RCJeep/app/src/main/java/com/nnikic/rcjeep/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         final Random r = new Random();
-        final TextView text = (TextView) findViewById(R.id.textViewZmija);
+        final TextView text = (TextView) findViewById(R.id.textView_1);
         final Button button = (Button) findViewById(R.id.buttonChangeColor);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -44,25 +44,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
---
+++ RCJeep/app/src/main/java/com/nnikic/rcjeep/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.nnikic.rcjeep;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nnikic on 4/6/2016.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        Thread splashTimer = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                    Intent mainIntent = new Intent("android.intent.action.MENU");
+                    startActivity(mainIntent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    finish();
+                }
+            }
+        };
+        splashTimer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
